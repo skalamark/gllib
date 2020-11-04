cb1c02bb6c6c9bb1ef1c8724c3f5d56e567804e3
JoseCarlosSkar <josecarlosskar@gmail.com>
2020-11-03 21:33:10 -0300

    create fn new for impl Lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -10,3 +10,26 @@ pub struct Lexer {
 	chars: Vec<char>,
 	textlines: Vec<String>,
 }
+
+impl Lexer {
+	pub fn new(source: String) -> Self {
+		let env: EnvLexer = EnvLexer {};
+		let mut textlines: Vec<String> = source.lines().map(|l: &str| l.to_string()).collect();
+		if textlines.len() == 0 {
+			textlines.push(source);
+		}
+		let mut chars: Vec<char> = textlines.remove(0).chars().map(|c: char| c).collect();
+		let cchar: String = if chars.len() > 0 {
+			chars.remove(0).to_string()
+		} else {
+			String::new()
+		};
+
+		Lexer {
+			env,
+			cchar,
+			chars,
+			textlines,
+		}
+	}
+}
