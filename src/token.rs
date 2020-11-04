55975d127187e13dbf78b2a81aaa2ddd5f1fa47c
JoseCarlosSkar <josecarlosskar@gmail.com>
2020-11-03 21:27:21 -0300

    create fn is_eof for impl Token

--- src/token.rs
+++ src/token.rs
@@ -5,6 +5,15 @@ pub enum Token {
 	Eof,
 }
 
+impl Token {
+	pub fn is_eof(&self) -> bool {
+		match self {
+			Token::Eof => true,
+			_ => false,
+		}
+	}
+}
+
 pub fn string_is_token(character: String) -> bool {
 	if SPACES.contains(&character.as_str()) {
 		true
