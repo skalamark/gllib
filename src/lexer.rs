ff1c6861adacac6b4abea2975e24a80d27ca8e07
JoseCarlosSkar <josecarlosskar@gmail.com>
2020-11-03 21:31:14 -0300

    create (struct EnvLexer, struct Lexer)

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,1 +1,12 @@
+use crate::token::{Token, SPACES};
 
+#[derive(Debug, PartialEq)]
+struct EnvLexer {}
+
+#[derive(Debug, PartialEq)]
+pub struct Lexer {
+	env: EnvLexer,
+	cchar: String,
+	chars: Vec<char>,
+	textlines: Vec<String>,
+}
