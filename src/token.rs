c0a67f47b9c596e03d481bc83ab6b510ef100f5a
JoseCarlosSkar <josecarlosskar@gmail.com>
2020-11-07 20:49:45 -0300

    create (const PUNCTUATIONS, field SemiColon for enum Token, fn is_semicolon for impl Token)

--- src/token.rs
+++ src/token.rs
@@ -1,14 +1,23 @@
 pub const SPACES: &str = "\r\n\t ";
+pub const PUNCTUATIONS: &str = ";";
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Token {
 	Eof,
+	SemiColon,
 }
 
 impl Token {
 	pub fn is_eof(&self) -> bool {
 		match self {
 			Token::Eof => true,
+			_ => false,
+		}
+	}
+
+	pub fn is_semicolon(&self) -> bool {
+		match self {
+			Token::SemiColon => true,
 			_ => false,
 		}
 	}
