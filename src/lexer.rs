38dde4ab83b0d3c2a54493310378aa90f2d0d86a
JoseCarlosSkar <josecarlosskar@gmail.com>
2020-11-11 17:12:27 -0300

    create (fn collision_check for impl Lexer, fn source_integers from mod tests) update (fns (new, make_token) from impl Lexer)

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,4 @@
-use crate::token::{Token, PUNCTUATIONS, SPACES};
+use crate::token::{string_is_token, Token, DIGITS, PUNCTUATIONS, SPACES};
 
 #[derive(Debug, PartialEq)]
 struct EnvLexer {}
@@ -14,7 +14,7 @@ pub struct Lexer {
 impl Lexer {
 	pub fn new(source: String) -> Self {
 		let env: EnvLexer = EnvLexer {};
-		let mut textlines: Vec<String> = source.lines().map(|l: &str| l.to_string()).collect();
+		let mut textlines: Vec<String> = source.lines().map(|l: &str| format!("{}\n", l)).collect();
 		if textlines.len() == 0 {
 			textlines.push(source);
 		}
@@ -70,12 +70,32 @@ impl Lexer {
 		}
 	}
 
+	fn collision_check(&self) -> Result<(), String> {
+		if self.cchar.is_empty()
+			|| SPACES.contains(&self.cchar.as_str())
+			|| PUNCTUATIONS.contains(&self.cchar.as_str())
+		{
+			Ok(())
+		} else {
+			if string_is_token(self.cchar.clone()) {
+				Err(format!("SyntaxError: invalid syntax"))
+			} else {
+				Err(format!("SyntaxError: invalid character in identifier"))
+			}
+		}
+	}
+
 	fn make_token(&mut self) -> Result<Token, String> {
 		if self.cchar.is_empty() {
 			Ok(Token::Eof)
 		} else if SPACES.contains(&self.cchar.as_str()) {
-			self.advance();
-			self.make_token()
+			if self.cchar == "\n" {
+				self.advance();
+				Ok(Token::NewLine)
+			} else {
+				self.advance();
+				self.make_token()
+			}
 		} else if PUNCTUATIONS.contains(&self.cchar.as_str()) {
 			if self.cchar == ";" {
 				self.advance();
@@ -83,6 +103,20 @@ impl Lexer {
 			}
 
 			self.make_token()
+		} else if DIGITS.contains(&self.cchar.as_str()) {
+			let mut number_literal: String = String::new();
+
+			while self.cchar.is_empty() == false && DIGITS.contains(&self.cchar.as_str()) {
+				number_literal.push_str(&self.cchar.as_str());
+				self.advance();
+			}
+
+			match self.collision_check() {
+				Ok(_) => {}
+				Err(details) => return Err(details),
+			}
+
+			Ok(Token::Integer(number_literal))
 		} else {
 			Err(format!("SyntaxError: invalid character in identifier"))
 		}
@@ -111,7 +145,7 @@ impl Lexer {
 mod tests {
 
 	use super::{EnvLexer, Lexer};
-	use crate::token::Token;
+	use crate::token::Token::*;
 
 	fn make_lexer(source: &str) -> Lexer {
 		Lexer::new(source.to_string())
@@ -132,19 +166,94 @@ mod tests {
 
 	#[test]
 	fn source_empty() {
-		assert_eq!(make_lexer("").run().unwrap(), vec![Token::Eof]);
+		assert_eq!(make_lexer("").run().unwrap(), vec![Eof]);
 	}
 
 	#[test]
 	fn source_spaces() {
-		assert_eq!(make_lexer("\r\n\t ").run().unwrap(), vec![Token::Eof]);
+		assert_eq!(make_lexer(" ").run().unwrap(), vec![NewLine, Eof]);
+		assert_eq!(make_lexer("\r").run().unwrap(), vec![NewLine, Eof]);
+		assert_eq!(make_lexer("\n").run().unwrap(), vec![NewLine, Eof]);
+		assert_eq!(make_lexer("\t").run().unwrap(), vec![NewLine, Eof]);
+
+		assert_eq!(
+			make_lexer("\r\n\t ").run().unwrap(),
+			vec![NewLine, NewLine, Eof]
+		);
 	}
 
 	#[test]
 	fn source_punctuations() {
+		fn punctuations() {
+			assert_eq!(
+				make_lexer(";").run().unwrap(),
+				vec![SemiColon, NewLine, Eof]
+			);
+		}
+
+		fn with_spaces() {
+			assert_eq!(
+				make_lexer("; \n;\r;\t;").run().unwrap(),
+				vec![SemiColon, NewLine, SemiColon, SemiColon, SemiColon, NewLine, Eof]
+			);
+		}
+
+		fn with_integers() {
+			assert_eq!(
+				make_lexer(";1;435;3457;987;").run().unwrap(),
+				vec![
+					SemiColon,
+					Integer(String::from("1")),
+					SemiColon,
+					Integer(String::from("435")),
+					SemiColon,
+					Integer(String::from("3457")),
+					SemiColon,
+					Integer(String::from("987")),
+					SemiColon,
+					NewLine,
+					Eof
+				]
+			);
+		}
+
+		punctuations();
+		with_spaces();
+		with_integers();
+	}
+
+	#[test]
+	fn source_integers() {
 		assert_eq!(
-			make_lexer(";").run().unwrap(),
-			vec![Token::SemiColon, Token::Eof]
+			make_lexer("0123456789").run().unwrap(),
+			vec![Integer(String::from("0123456789")), NewLine, Eof]
+		);
+
+		assert_eq!(
+			make_lexer(" 834\n23894\r3948\t98735 345 ").run().unwrap(),
+			vec![
+				Integer(String::from("834")),
+				NewLine,
+				Integer(String::from("23894")),
+				Integer(String::from("3948")),
+				Integer(String::from("98735")),
+				Integer(String::from("345")),
+				NewLine,
+				Eof
+			]
+		);
+
+		assert_eq!(
+			make_lexer(";012345;6789;").run().unwrap(),
+			vec![
+				SemiColon,
+				Integer(String::from("012345")),
+				SemiColon,
+				Integer(String::from("6789")),
+				SemiColon,
+				NewLine,
+				Eof
+			]
 		);
 	}
 }
